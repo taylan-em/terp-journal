812a47254b8df722177eb3cc5d34ea35f5d50825
taylan-em <emresoyt@gmail.com>
2026-05-10 15:25:05 +1000

    remove test endpoint

--- api/test.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const key = process.env.ANTHROPIC_API_KEY;
-  res.json({ 
-    hasKey: !!key, 
-    keyStart: key ? key.substring(0,12) : 'missing'
-  });
-}
